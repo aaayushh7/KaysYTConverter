746763cba796b80928a131be6e1da373175085bc
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-03 21:51:22 +0530

    error handled upon api crash

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,26 @@ import axios from "axios";
 import { useRef, useState } from "react";
 import { youtube_parser } from "./utils";
 
-
 function App() {
 
   const inputUrlRef = useRef();
   const [urlResult, setUrlResult] = useState();
+  const [isLoading, setIsLoading] = useState(false); // New state for loading
+  const [isError, setIsError] = useState(false); // New state for error
+  const [isEmpty, setIsEmpty] = useState(false); // New state for empty input
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const youtubeID = youtube_parser(inputUrlRef.current.value)
+    setIsLoading(true); // Set loading to true when the form is submitted
+    setIsError(false); // Reset error state
+    setIsEmpty(false); // Reset empty state
+    const inputValue = inputUrlRef.current.value;
+    if (!inputValue) {
+      setIsEmpty(true); // Set empty state if input is empty
+      setIsLoading(false); // Set loading to false
+      return;
+    }
+    const youtubeID = youtube_parser(inputValue)
     console.log(youtubeID)
 
     const options = {
@@ -25,8 +36,19 @@ function App() {
       }
     }
     axios(options)
-      .then(res => setUrlResult(res.data.link))
-      .catch(err => console.log(err))
+      .then(res => {
+        if (res.data.link) {
+          setUrlResult(res.data.link);
+        } else {
+          setIsError(true); // Set error state if result is empty
+        }
+        setIsLoading(false); // Set loading to false when the result is received
+      })
+      .catch(err => {
+        console.log(err);
+        setIsError(true); // Set error state if there is an error
+        setIsLoading(false); // Set loading to false even if there is an error
+      })
 
     inputUrlRef.current.value = '';
   }
@@ -34,9 +56,6 @@ function App() {
   return (
     <>
       <div className="background">
-        <span></span>
-        <span></span>
-        <span></span>
         <span></span>
         <span></span>
         <span></span>
@@ -67,10 +86,12 @@ function App() {
 
           <form onSubmit={handleSubmit} className="form">
             <input ref={inputUrlRef} placeholder="Paste youtube URL here..." className="form_input" type="text" />
-            <button type="submit" className="button-74">Convert</button>
+            <button type="submit" className="button-74">{isLoading ? 'Loading...' : 'Convert'}</button>
           </form>
 
           {urlResult ? <a target="_blank" rel="noreferrer" href={urlResult}><button className="button-30">Download mp3</button></a> : ''}
+          {isError && <p>Oops! sorry an error ate me. Give me another shot!🥺</p>} 
+          {isEmpty && <p>Please enter a YouTube URL.</p>} 
 
         </section>
 
